3b3bf011835eac2104eec5aff9397c0eb4d6a251
techjini <techjini@TJ-MB-070.local>
2019-02-06 13:50:17 +0530

    Initial Commit

---
+++ GiphySample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySample
+//
+//  Created by techjini on 06/02/19.
+//  Copyright © 2019 techjini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
